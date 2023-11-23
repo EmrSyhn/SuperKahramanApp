08198cca0b5c18ef3ff0a62008fee1975ad057c3
Emir <emirseyhana.z@gmail.com>
2023-11-23 14:40:30 +0300

    Initial Commit

---
+++ SuperKahraman/SuperKahramanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperKahramanApp.swift
+//  SuperKahraman
+//
+//  Created by Emir Seyhan on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperKahramanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
